8a4dc2753b24cc23773a21ac01ecc511e9ff530f
jungwoonAn <red0808@naver.com>
2022-04-21 12:41:23 +0900

    project1_220421

---
+++ js/photo_change.js
@@ -0,0 +1,6 @@
+$(function(){
+	bt1.onclick=function() {cimg.src="img/sub_m.jpg";}
+    bt2.onclick=function() {cimg.src="img/sub_m2.jpg";}
+    bt3.onclick=function() {cimg.src="img/sub_m3.jpg";}
+    bt4.onclick=function() {cimg.src="img/sub_m4.jpg";}
+});
